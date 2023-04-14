413fe180a0b5cdfcf51ab9f9f062c52cf679a9c7
Muyan Xie <1756314803@qq.com>
2023-04-14 01:28:26 -0500

    update comment component

--- src/components/test/Testdetailedpost.jsx
+++ src/components/test/Testdetailedpost.jsx
@@ -9,6 +9,7 @@ const Testdetailedpost = () => {
 
   const likeHandler = () => {
     setLike(!like);
+    // need to add a like to the database
   };
 
   return (
@@ -29,9 +30,10 @@ const Testdetailedpost = () => {
           <br></br>
           <div className={classes.or}></div>
           <br></br>
-          <div>
+          <div style={{ display: "flex", alignItems: "center" }}>
             <button
               className={classes.transparent_button}
+              style={{ marginRight: "50px" }}
               onClick={likeHandler}
             >
               <svg
@@ -45,13 +47,38 @@ const Testdetailedpost = () => {
                 stroke-linecap="round"
                 stroke-linejoin="round"
                 role="button"
-                fill = {like ? "red" : "none"}
+                fill={like ? "red" : "none"}
               >
                 <path stroke="none" d="M0 0h24v24H0z" fill="none" />
                 <path d="M19.5 12.572l-7.5 7.428l-7.5 -7.428a5 5 0 1 1 7.5 -6.566a5 5 0 1 1 7.5 6.572" />
               </svg>
             </button>
+            <button className={classes.transparent_button}>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                class="icon icon-tabler icon-tabler-message-circle"
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+                stroke-width="2"
+                stroke="currentColor"
+                fill="none"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                role="button"
+              >
+                <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                <path d="M3 20l1.3 -3.9c-2.324 -3.437 -1.426 -7.872 2.1 -10.374c3.526 -2.501 8.59 -2.296 11.845 .48c3.255 2.777 3.695 7.266 1.029 10.501c-2.666 3.235 -7.615 4.215 -11.574 2.293l-4.7 1" />
+              </svg>
+            </button>
           </div>
+          <br></br>
+          <div className={classes.or}></div>
+          <br></br>
+
+
+
+
         </div>
       )}
     </div>
@@ -59,5 +86,3 @@ const Testdetailedpost = () => {
 };
 
 export default Testdetailedpost;
-
-// style={{background: "white", border: '0'}}
