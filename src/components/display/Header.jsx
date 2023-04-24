6de830c272b549877d30de5f7a1a8df61315e307
Muyan Xie <1756314803@qq.com>
2023-04-23 23:50:41 -0500

    update the process

--- src/components/display/Header.jsx
+++ src/components/display/Header.jsx
@@ -33,7 +33,7 @@ const Header = ({ name }) => {
         <Navbar.Collapse id="navbarScroll">
           <Nav
             className="me-auto my-2 my-lg-0"
-            style={{ maxHeight: "100px" }}
+            style={{ maxHeight: "100px", fontSize:"large" }}
             navbarScroll
           >
             <Nav.Link href="#feed">Feed</Nav.Link>
