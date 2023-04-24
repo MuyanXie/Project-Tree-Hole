75a2c374e7e61246373438331ceebfa7a5ab196c
Muyan Xie <1756314803@qq.com>
2023-04-24 10:37:16 -0500

    add the website info page

---
+++ src/components/display/About.jsx
@@ -0,0 +1,47 @@
+import React from "react";
+import Header from "./Header";
+import background from "../../static/pics/UChicago_Dusk.jpeg";
+import classes from "./About.module.css";
+
+const About = () => {
+  return (
+    <div>
+      <Header name={"About"} />
+      <div>
+        <img
+          src={background}
+          alt="UChicago Dusk"
+          width={"100%"}
+          height={"100%"}
+          style={{ position: "relative", zIndex: "-1" }}
+        />
+        <div
+          style={{
+            zIndex: "1",
+            left: "50%",
+            padding: "5px",
+            top: "50%",
+            transform: "translate(-50%, -50%)",
+            color: "white",
+            position: "absolute",
+          }}
+          className={classes.block}
+        >
+          <h1 style={{ fontSize: "xxx-large", fontFamily: "Times New Roman" }}>
+            University of Chicago Tree Hole
+          </h1>
+          <h5 style={{ alignItems: "center", fontSize: "xx-large", fontFamily: "Times New Roman" }}>
+            Honorly built by the UChicago Tree Hole Team
+          </h5>
+          <br></br>
+          <h5 style={{ alignItems: "center", fontSize: "xx-large", fontFamily: "Times New Roman"
+        }}>
+            "We are here to listen and to find the way out"
+          </h5>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default About;
